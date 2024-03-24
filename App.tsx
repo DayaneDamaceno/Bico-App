4ef96b3e20d4582fcdaa4a387fd964a7790b3fa6
Dayane Damaceno <Dayane.Damaceno@t-systems.com>
2024-03-23 22:17:17 -0300

    [STYLE] Componente de categoria

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Categoria } from "./components/Categoria";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Dayane Wesley e dudu</Text>
+      <Button title="Clique Aqui" />
       <StatusBar style="auto" />
+      <View style={styles.listaCategorias}>
+        <Categoria icon="icon" nome="Reformas e raparos" />
+        <Categoria nome="nome 2" icon="icon 2" />
+        <Categoria nome="nome 3" icon="icon 3" />
+        <Categoria nome="nome 4" icon="icon 4" />
+      </View>
     </View>
   );
 }
@@ -13,8 +21,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listaCategorias: {
+    gap: 10,
+    flexDirection: "row",
+    // flexWrap: "wrap",
   },
 });
---
+++ components/Categoria.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+
+interface CategoriaProps {
+  icon?: string;
+  nome: string;
+}
+
+export function Categoria(props: CategoriaProps) {
+  return (
+    <View style={styles.categoria}>
+      <Fontisto name="home" size={24} color="black" />
+      <Text>{props.nome}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  categoria: {
+    backgroundColor: "#7C3AED",
+    // width: "50%",
+  },
+});
